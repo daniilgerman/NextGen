fb343ce7404fee9140bcf8b8b5586b27c088bcc6
Daniil263 <d.ger645@mail.ru>
2020-03-19 12:33:40 +0700

    Payment Money

---
+++ WindowsProject1/Business logic/Payment.cpp
@@ -0,0 +1,12 @@
+#include "Payment.h"
+
+Payment::Payment(Money cashTendered)
+{
+amount = cashTendered;
+}
+
+
+Money Payment::getAmount()
+{
+	return amount;
+}
--- WindowsProject1/Business logic/Payment.h
+++ WindowsProject1/Business logic/Payment.h
@@ -1,1 +1,9 @@
 #pragma once
+class Payment
+{
+private:
+	Money amount;
+public:
+	Payment(Money cachTendered);
+	Money getAmount();
+};
